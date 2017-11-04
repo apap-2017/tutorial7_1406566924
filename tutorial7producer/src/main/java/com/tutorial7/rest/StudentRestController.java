cab525a1abe5496dc68a5e87bf89843accb86ed1
Manda <imanda.minara@outlook.com>
2017-11-04 20:24:39 +0700

    implemen latihan 1

--- tutorial7producer/src/main/java/com/tutorial7/rest/StudentRestController.java
+++ tutorial7producer/src/main/java/com/tutorial7/rest/StudentRestController.java
@@ -1,5 +1,7 @@
 package com.tutorial7.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,14 @@ public class StudentRestController {
 	StudentService studentDAO;
 	
 	@RequestMapping("/student/view/{npm}")
-	public StudentModel view (@PathVariable(value = "npm" ) String npm)  {
+	public StudentModel view(@PathVariable(value = "npm") String npm)  {
 		StudentModel student = studentDAO.selectStudent(npm);
 		return student;
+	}
+	
+	@RequestMapping("/student/viewall")
+	public List<StudentModel> viewall()  {
+		List<StudentModel> students = studentDAO.selectAllStudents();
+		return students;
 	}
 }
